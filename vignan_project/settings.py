23d18df5ff16a0c4f814cf2f924fa59281fcd893
akilthangavel1 <120370450+akilthangavel1@users.noreply.github.com>
2026-05-30 11:08:50 +0530

    Allow all hosts in settings

--- vignan_project/settings.py
+++ vignan_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nogcw()&&r47494cs)vd_^2zd=34yndo^zg8797froawz^nx8$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
